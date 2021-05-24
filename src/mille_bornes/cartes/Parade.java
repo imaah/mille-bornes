6117b6ffaa34dcf9b1d024f8b7dcc53702d56de1
Esteban Morisse <68004644+myimah@users.noreply.github.com>
2021-05-24 22:04:53 +0200

    ajout d'un attribut Carte#imagePath qui pointe vers le fichier image d'une image et d'une carte par défaut (qui représente une carte retournée.

---
+++ src/mille_bornes/cartes/Parade.java
@@ -0,0 +1,31 @@
+package mille_bornes.cartes;
+
+import mille_bornes.EtatJoueur;
+import mille_bornes.Jeu;
+
+public abstract class Parade extends Bataille {
+    private static final long serialVersionUID = -1307910562516914625L;
+
+    public Parade(String nom, String imagePath) {
+        super("\u001B[92m" + nom + "\u001B[0m", Categorie.PARADE, imagePath);
+    }
+
+    @Override
+    public void appliqueEffet(Jeu jeu, EtatJoueur joueur) {
+        Bataille bataille = joueur.getBataille();
+
+        // Si le joueur a bien une attaque
+        if (bataille instanceof Attaque) {
+            if (contre((Attaque) bataille)) {
+                joueur.defausseBataille(jeu);
+            } else {
+                // Sinon on ne peut pas la parer
+                throw new IllegalStateException("Cette carte ne peut pas être jouée maintenant");
+            }
+        } else {
+            // On a rien à parer
+            throw new IllegalStateException("Cette carte ne peut pas être jouée maintenant");
+        }
+        joueur.setBataille(this);
+    }
+}
