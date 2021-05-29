d889baa612d87f1c7019b10c6afc7b466a42dcb3
Esteban Morisse <68004644+myimah@users.noreply.github.com>
2021-05-29 17:13:49 +0200

    Ajout d'une constante définissant la taille de la fenêtre

--- src/mille_bornes/App.java
+++ src/mille_bornes/App.java
@@ -7,20 +7,23 @@ import mille_bornes.vue.MilleBornes;
 
 public class App extends Application {
 
+    private static final int APP_WIDTH = 1200;
+    private static final int APP_HEIGHT = 1000;
+
     public static void main(String[] args) {
         Application.launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        MilleBornes app = new MilleBornes(1200, 1000);
+        MilleBornes app = new MilleBornes(APP_WIDTH, APP_HEIGHT);
 
         Scene scene = new Scene(app.getHolder());
         stage.setScene(scene);
         stage.setTitle("1000 bornes");
         stage.setResizable(false);
-        stage.setWidth(1200);
-        stage.setHeight(1000);
+        stage.setWidth(APP_WIDTH);
+        stage.setHeight(APP_HEIGHT);
         stage.show();
     }
 }
