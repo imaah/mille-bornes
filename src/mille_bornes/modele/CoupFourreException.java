abf45118ee170490cfb71a98746b13c048ae8c82
Clément Guibout <clement.guibout@gmail.com>
2021-06-05 23:54:59 +0200

    Fix & commentaires de CoupFourreException.java

--- src/mille_bornes/modele/CoupFourreException.java
+++ src/mille_bornes/modele/CoupFourreException.java
@@ -1,13 +1,15 @@
 package mille_bornes.modele;
 
 public class CoupFourreException extends RuntimeException {
+    /** La cible en question */
     private final Joueur cible;
 
+    /**
+     * Permet de préciser une cible à viser
+     *
+     * @param cible La cible en question
+     */
     public CoupFourreException(Joueur cible) {
         this.cible = cible;
-    }
-
-    public Joueur getCible() {
-        return cible;
     }
 }
